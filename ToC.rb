0c0b01b397d33e00499a7a9e7421659a8c401e83
Scott Sekerke <scottsek@gmail.com>
2013-12-31 11:21:40 +0200

    Chapt 6, table of contents, simple version

---
+++ ToC.rb
@@ -0,0 +1,11 @@
+#Table of Contents
+line_width = 50
+chap1_title = 'Chapter 1:  Getting Started'
+chap2_title = 'Chapter 2:  Numbers'
+chap3_title = 'Chapter 3:  Letters'
+chap1_page = 'page  1'
+chap2_page = 'page  9'
+chap3_page = 'page 13'
+puts chap1_title.ljust(line_width*4/5) + chap1_page.rjust(line_width*1/5)
+puts chap2_title.ljust(line_width*4/5) + chap2_page.rjust(line_width*1/5)
+puts chap3_title.ljust(line_width*4/5) + chap3_page.rjust(line_width*1/5)
