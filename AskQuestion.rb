3e79926896cb67242ec5dec8a8ee4a5e4b4b2671
Scott Sekerke <scottsek@gmail.com>
2014-01-03 21:24:36 +0200

    Last entry before class

---
+++ AskQuestion.rb
@@ -0,0 +1,19 @@
+def ask question
+while true
+  puts question
+  reply = gets.chomp.downcase
+	if (reply == 'yes' || reply == 'no')
+		if reply == 'yes'
+			true
+			return
+		else
+    	false
+    	return
+		end
+		break
+	else
+    puts 'Please answer "yes" or "no".'
+	end
+end # This is what we return (true or false).
+end
+puts ask 'Do you like me?'
---
+++ wordsort.rb
@@ -0,0 +1,8 @@
+words = Array.new
+puts 'Type in one word per line, to stop, press Enter/Return on a blank line:'
+user_word = 'slug'
+while user_word.length != 0
+	user_word = gets.chomp
+	words.push user_word
+end
+puts words.sort
