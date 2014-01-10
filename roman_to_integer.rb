2e247d31b7bd404e8a8839d640ad83cbba3dcec5
scottsek <ashicap@server.ashicap.com>
2014-01-10 14:47:12 +0200

    roman to integer

---
+++ roman_to_integer.rb
@@ -0,0 +1,19 @@
+roman_to_integer = {'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000}
+previous = 0
+amount = 0 
+puts 'Please input a roman number:'
+user_response = gets.chomp
+user_response.each_char do |c|
+	current = roman_to_integer[c.upcase]
+	if current.nil?
+		put "#{c} is not a valid Roman numeral"
+	end
+	if current < previous
+		add_me = (-1 * current)
+	else
+		add_me = current
+	end
+	previous = current
+	amount += add_me
+end
+puts amount
